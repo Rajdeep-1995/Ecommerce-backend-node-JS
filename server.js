ced0eeb4486b4c4f1447f2e27928f6668bea0075
Rajdeep Sondarva <rsondarva71@gmail.com>
2021-05-26 20:59:54 -0400

    increasing JSON limit for incoming response

--- server.js
+++ server.js
@@ -2,33 +2,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
-const {readdirSync} = require("fs");
+const { readdirSync } = require("fs");
 require("dotenv").config();
 
 const app = express();
 
-
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useFindAndModify:true,
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
     useUnifiedTopology: true,
-    useFindAndModify:false
-})
-.then(()=>console.log(`Database connection successful`))
-.catch(err=>console.log(err))
+    useFindAndModify: false,
+  })
+  .then(() => console.log(`Database connection successful`))
+  .catch((err) => console.log(err));
 
 //middelwares
 app.use(morgan("dev"));
-app.use(express.urlencoded({extended:true}))
-app.use(express.json({limit:"5mb"}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 
 //reading all routes from route directory
-readdirSync("./routes").map((r)=>app.use("/api",require("./routes/"+r)))
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
-const port = process.env.PORT||8000;
+const port = process.env.PORT || 8000;
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
